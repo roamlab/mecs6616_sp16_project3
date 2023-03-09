c4d89cb4158ee1e1543fc63aa567aa387976f13b
Jingxi Xu <zealliant@outlook.com>
2023-03-09 02:02:14 -0500

    update

--- score.py
+++ score.py
@@ -1,7 +1,9 @@
 import numpy as np
-from arm_gui import ArmGUI, Renderer
+# from arm_gui import ArmGUI, Renderer
+from student_code.project3.render import Renderer
 import time
 import math
+
 np.set_printoptions(suppress=True)
 
 
@@ -28,14 +30,16 @@ def set_torque0(arm_teacher, arm_student, torque):
 
 
 def score_random_torque(arm_teacher, arm_student, gui):
+    np.random.seed(10)
     time_limit = 5
     num_tests = 50
 
+    mses = []
     scores = []
     torques = np.random.uniform(-1.5, 1.5, num_tests)
     for i, torque in enumerate(torques):
         print("\n----------------------------------------")
-        print(f'TEST {i+1} ( Torque = {torque} Nm)\n')
+        print(f'TEST {i+1} (Torque = {torque} Nm)\n')
         reset(arm_teacher, arm_student, torque)
 
         if gui:
@@ -49,36 +53,87 @@ def score_random_torque(arm_teacher, arm_student, gui):
             arm_student.advance()
             if gui:
                 renderer.plot([(arm_teacher, 'tab:blue'), (arm_student, 'tab:red')])
-                # time.sleep(max(0, dt - (time.time() - t)))
             mse = ((arm_student.get_state() - arm_teacher.get_state())**2).mean()
             mse_list.append(mse)
 
-        if gui:
-            renderer.plot(None)
-            time.sleep(2)
-
         mse = np.array(mse_list).mean()
+        mses.append(mse)
         print(f'average mse: {mse}')
-        score = 1 if mse < 0.008 else 0
+        if mse < 0.008:
+          score = 0.5
+        if mse < 0.0005:
+          score = 1
+        if mse >= 0.008:
+          score = 0
         scores.append(score)
         print(f'Score: {score}/{1}')
         print("----------------------------------------\n")
 
     print("\n----------------------------------------")
-    print(f'Final Score: {np.array(scores).sum()}/{50} = {np.array(scores).sum()/50:.2f}')
+    print(f'Final Score: {np.array(scores).sum()}/{50}*{5} = {np.array(scores).sum()/50*5:.2f}')
     print("----------------------------------------\n")
+    # print(max(mses))
 
 
-def score_two_torques(arm_teacher, arm_student, gui):
+def score_linear_torques(arm_teacher, arm_student, gui):
+    np.random.seed(10)
     time_limit = 5
     num_tests = 50
 
+    mses = []
+    scores = []
+    torques = np.random.uniform(0.5, 1.5, num_tests)
+    for i, torque in enumerate(torques):
+        print("\n----------------------------------------")
+        print(f'TEST {i+1} (Torque 0 -> {torque} Nm)\n')
+        reset(arm_teacher, arm_student, 0)
+
+        if gui:
+            renderer = Renderer()
+            time.sleep(1)
+
+        mse_list = []
+        while arm_teacher.get_t() < time_limit:
+            t = time.time()
+            set_torque0(arm_teacher, arm_student, arm_teacher.get_t() / time_limit * torque)
+            arm_teacher.advance()
+            arm_student.advance()
+            if gui:
+                renderer.plot([(arm_teacher, 'tab:blue'), (arm_student, 'tab:red')])
+            mse = ((arm_student.get_state() - arm_teacher.get_state())**2).mean()
+            mse_list.append(mse)
+
+        mse = np.array(mse_list).mean()
+        mses.append(mse)
+        print(f'average mse: {mse}')
+        if mse < 0.008:
+          score = 0.5
+        if mse < 0.0005:
+          score = 1
+        if mse >= 0.008:
+          score = 0
+        scores.append(score)
+        print(f'Score: {score}/{1}')
+        print("----------------------------------------\n")
+
+    print("\n----------------------------------------")
+    print(f'Final Score: {np.array(scores).sum()}/{50}*{5} = {np.array(scores).sum()/50*5:.2f}')
+    print("----------------------------------------\n")
+    # print(max(mses))
+
+
+def score_two_torques(arm_teacher, arm_student, gui):
+    np.random.seed(10)
+    time_limit = 5
+    num_tests = 50
+
+    mses = []
     scores = []
     torques1 = np.random.uniform(-1, 1, num_tests)
     torques2 = np.random.uniform(-1, 1, num_tests)
     for i, (torque1, torque2) in enumerate(zip(torques1, torques2)):
         print("\n----------------------------------------")
-        print(f'TEST {i+1} ( Torque 1 = {torque1} Nm,  Torque 1 = {torque2} Nm)\n')
+        print(f'TEST {i+1} (Torque 1 = {torque1} Nm,  Torque 2 = {torque2} Nm)\n')
         reset(arm_teacher, arm_student, 0)
 
         if gui:
@@ -96,64 +151,23 @@ def score_two_torques(arm_teacher, arm_student, gui):
             arm_student.advance()
             if gui:
                 renderer.plot([(arm_teacher, 'tab:blue'), (arm_student, 'tab:red')])
-                # time.sleep(max(0, dt - (time.time() - t)))
             mse = ((arm_student.get_state() - arm_teacher.get_state())**2).mean()
             mse_list.append(mse)
 
-        if gui:
-            renderer.plot(None)
-            time.sleep(2)
-
         mse = np.array(mse_list).mean()
+        mses.append(mse)
         print(f'average mse: {mse}')
-        score = 1 if mse < 0.008 else 0
+        if mse < 0.05:
+          score = 0.5
+        if mse < 0.01:
+          score = 1
+        if mse >= 0.05:
+          score = 0
         scores.append(score)
         print(f'Score: {score}/{1}')
         print("----------------------------------------\n")
 
     print("\n----------------------------------------")
-    print(f'Final Score: {np.array(scores).sum()}/{50} = {np.array(scores).sum()/50:.2f}')
+    print(f'Final Score: {np.array(scores).sum()}/{50}*{5} = {np.array(scores).sum()/50*5:.2f}')
     print("----------------------------------------\n")
-
-
-def score_linear_torques(arm_teacher, arm_student, gui):
-    time_limit = 5
-    num_tests = 50
-
-    scores = []
-    torques = np.random.uniform(0.5, 1.5, num_tests)
-    for i, torque in enumerate(torques):
-        print("\n----------------------------------------")
-        print(f'TEST {i+1} ( Torque 0 -> {torque} Nm )\n')
-        reset(arm_teacher, arm_student, 0)
-
-        if gui:
-            renderer = Renderer()
-            time.sleep(1)
-
-        mse_list = []
-        while arm_teacher.get_t() < time_limit:
-            t = time.time()
-            set_torque0(arm_teacher, arm_student, arm_teacher.get_t() / time_limit * torque)
-            arm_teacher.advance()
-            arm_student.advance()
-            if gui:
-                renderer.plot([(arm_teacher, 'tab:blue'), (arm_student, 'tab:red')])
-                # time.sleep(max(0, dt - (time.time() - t)))
-            mse = ((arm_student.get_state() - arm_teacher.get_state())**2).mean()
-            mse_list.append(mse)
-
-        if gui:
-            renderer.plot(None)
-            time.sleep(2)
-
-        mse = np.array(mse_list).mean()
-        print(f'average mse: {mse}')
-        score = 1 if mse < 0.008 else 0
-        scores.append(score)
-        print(f'Score: {score}/{1}')
-        print("----------------------------------------\n")
-
-    print("\n----------------------------------------")
-    print(f'Final Score: {np.array(scores).sum()}/{50} = {np.array(scores).sum()/50:.2f}')
-    print("----------------------------------------\n")
+    # print(max(mses))
